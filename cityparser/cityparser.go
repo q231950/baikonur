0dc34c7c74e62f61ee3529bc8bb88946068a361f
Martin Kim Dung-Pham <kim@elbedev.com>
2017-06-25 18:38:52 +0200

    Change CityParser format

--- cityparser/cityparser.go
+++ cityparser/cityparser.go
@@ -10,8 +10,10 @@ import (
 	"net/http"
 	"strconv"
 	"sync"
+	"time"
 
 	log "github.com/Sirupsen/logrus"
+	"github.com/q231950/baikonur/model"
 	"github.com/q231950/sputnik/keymanager"
 	"github.com/q231950/sputnik/requesthandling"
 )
@@ -23,6 +25,7 @@ type CityParser struct {
 // Parse parses the file given input
 func (p CityParser) Parse(reader io.Reader) {
 	r := csv.NewReader(reader)
+	r.Comma = ';'
 
 	recordChannel := make(chan []string, 10)
 	wg := new(sync.WaitGroup)
@@ -36,10 +39,11 @@ func (p CityParser) Parse(reader io.Reader) {
 		}
 		if err != nil {
 			log.Fatal(err)
+		} else {
+			wg.Add(1)
+			recordChannel <- record
 		}
 
-		wg.Add(1)
-		recordChannel <- record
 	}
 
 	wg.Wait()
@@ -47,35 +51,57 @@ func (p CityParser) Parse(reader io.Reader) {
 	close(recordChannel)
 }
 
+// The main 'geoname' table has the following fields :
+// ---------------------------------------------------
+// geonameid         : integer id of record in geonames database
+// name              : name of geographical point (utf8) varchar(200)
+// asciiname         : name of geographical point in plain ascii characters, varchar(200)
+// alternatenames    : alternatenames, comma separated, ascii names automatically transliterated, convenience attribute from alternatename table, varchar(10000)
+// latitude          : latitude in decimal degrees (wgs84)
+// longitude         : longitude in decimal degrees (wgs84)
+// feature class     : see http://www.geonames.org/export/codes.html, char(1)
+// feature code      : see http://www.geonames.org/export/codes.html, varchar(10)
+// country code      : ISO-3166 2-letter country code, 2 characters
+// cc2               : alternate country codes, comma separated, ISO-3166 2-letter country code, 200 characters
+// admin1 code       : fipscode (subject to change to iso code), see exceptions below, see file admin1Codes.txt for display names of this code; varchar(20)
+// admin2 code       : code for the second administrative division, a county in the US, see file admin2Codes.txt; varchar(80)
+// admin3 code       : code for third level administrative division, varchar(20)
+// admin4 code       : code for fourth level administrative division, varchar(20)
+// population        : bigint (8 byte int)
+// elevation         : in meters, integer
+// dem               : digital elevation model, srtm3 or gtopo30, average elevation of 3''x3'' (ca 90mx90m) or 30''x30'' (ca 900mx900m) area in meters, integer. srtm processed by cgiar/ciat.
+// timezone          : the iana timezone id (see file timeZone.txt) varchar(40)
+// modification date : date of last modification in yyyy-MM-dd format
 func (p CityParser) insertCity(recordChannel chan []string, wg *sync.WaitGroup) {
 
-	for record := range recordChannel {
-		type City struct {
-			CountryCode, City, AccentCity, Region string
-			Population                            int
-			Latitude                              string
-			Longitude                             string
-		}
+	client := &http.Client{}
 
-		populationInt, _ := strconv.Atoi(record[4])
-		// latitudeFloat, _ := strconv.ParseFloat(record[5], 64)
-		// longitudeFloat, _ := strconv.ParseFloat(record[6], 64)
-		city := City{
-			record[0],
-			record[1],
-			record[2],
-			record[3],
-			populationInt,
-			record[5],
-			record[6]}
-		log.WithFields(log.Fields{
-			"Country code": city.CountryCode,
-			"City":         city.City,
-			"Accent city":  city.AccentCity,
-			"Region":       city.Region,
-			"Population":   city.Population,
-			"Latitude":     city.Latitude,
-			"Longitude":    city.Longitude}).Info("record")
+	for record := range recordChannel {
+		log.Info(record)
+		population, _ := strconv.Atoi(record[14])
+		elevation, _ := strconv.Atoi(record[15])
+		latitude, _ := strconv.ParseFloat(record[4], 64)
+		longitude, _ := strconv.ParseFloat(record[5], 64)
+
+		city := model.City{
+			GeoNameID:      record[0],
+			Name:           record[1],
+			ASCIIName:      record[2],
+			AlternateNames: record[3],
+			Latitude:       latitude,
+			Longitude:      longitude,
+			FeatureClass:   record[6],
+			FeatureCode:    record[7],
+			CountryCode:    record[8],
+			CC2:            record[9],
+			AdminCode1:     record[10],
+			AdminCode2:     record[11],
+			AdminCode3:     record[12],
+			AdminCode4:     record[13],
+			Population:     int64(population),
+			Elevation:      int64(elevation),
+			DEM:            record[16],
+			Timezone:       record[17]}
 
 		keyManager := keymanager.New()
 		containerID := "iCloud.com.elbedev.bish"
@@ -84,38 +110,73 @@ func (p CityParser) insertCity(recordChannel chan []string, wg *sync.WaitGroup) 
 		database := "public"
 		requestManager := requesthandling.New(config, &keyManager, database)
 		tmpl, err := template.New("test").Parse(`{
-		      "operations": [
-		          {
-		              "operationType": "create",
-		              "record": {
-		                  "recordType": "city",
-		                  "fields": {
-                          "country_code": {
-                            "value": "{{.CountryCode}}"
-                          },
-                          "accent_city":{
-                            "value": "{{.AccentCity}}"
-                          },
-		                      "name": {
-		                          "value": "{{.City}}"
-		                      },
-                          "region": {
-                            "value": "{{.Region}}"
-                          },
-                          "location": {
-                            "value": {
-                              "latitude": {{.Latitude}},
-                              "longitude": {{.Longitude}}
-                            }
-                          },
-                          "population": {
-                            "value": {{.Population}}
-                          }
-		                  }
-		              }
-		          }
-		      ]
-		  }`)
+			      "operations": [
+			          {
+			              "operationType": "create",
+			              "record": {
+			                  "recordType": "cities",
+			                  "fields": {
+		                        "geonameid": {
+		                          "value": "{{.GeoNameID}}"
+		                        },
+														"name": {
+		                          "value": "{{.Name}}"
+		                        },
+			                      "asciiname": {
+			                          "value": "{{.ASCIIName}}"
+			                      },
+		                        "alternatenames": {
+		                          "value": "{{.AlternateNames}}"
+		                        },
+		                        "location": {
+		                          "value": {
+		                            "latitude": {{.Latitude}},
+		                            "longitude": {{.Longitude}}
+		                          }
+		                        },
+		                        "feature_class": {
+		                          "value": "{{.FeatureClass}}"
+		                        },
+		                        "feature_code": {
+		                          "value": "{{.FeatureCode}}"
+		                        },
+		                        "country_code": {
+		                          "value": "{{.CountryCode}}"
+		                        },
+		                        "cc2": {
+		                          "value": "{{.CC2}}"
+		                        },
+		                        "admin1_code": {
+		                          "value": "{{.AdminCode1}}"
+		                        },
+		                        "admin2_code": {
+		                          "value": "{{.AdminCode2}}"
+		                        },
+		                        "admin3_code": {
+		                          "value": "{{.AdminCode3}}"
+		                        },
+		                        "admin4_code": {
+		                          "value": "{{.AdminCode4}}"
+		                        },
+		                        "population": {
+		                          "value": {{.Population}}
+		                        },
+		                        "elevation": {
+		                          "value": {{.Elevation}}
+		                        },
+		                        "dem": {
+		                          "value": "{{.DEM}}"
+		                        },
+		                        "timezone": {
+		                          "value": "{{.Timezone}}"
+		                        }
+			                  }
+			              }
+			          }
+			      ]
+			  }`)
+
+		log.Info(tmpl)
 		if err != nil {
 			panic(err)
 		}
@@ -132,7 +193,6 @@ func (p CityParser) insertCity(recordChannel chan []string, wg *sync.WaitGroup) 
 			log.Fatal("Failed to create ping request")
 		}
 
-		client := &http.Client{}
 		resp, err := client.Do(request)
 		if err != nil {
 			panic(err)
@@ -143,6 +203,9 @@ func (p CityParser) insertCity(recordChannel chan []string, wg *sync.WaitGroup) 
 		fmt.Println("response Headers:", resp.Header)
 		responseBody, _ := ioutil.ReadAll(resp.Body)
 		fmt.Println("response Body:", string(responseBody))
-		wg.Done()
+		go func() {
+			time.Sleep(time.Millisecond * 10)
+			wg.Done()
+		}()
 	}
 }
