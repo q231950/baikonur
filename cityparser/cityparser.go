3e3c454df5aaa991a8a61691d6696c7397ce79ea
Martin Kim Dung-Pham <kim@elbedev.com>
2017-06-25 16:31:11 +0200

    Update parser

--- cityparser/cityparser.go
+++ cityparser/cityparser.go
@@ -1,11 +1,15 @@
 package cityparser
 
 import (
+	"bytes"
 	"encoding/csv"
 	"fmt"
+	"html/template"
 	"io"
 	"io/ioutil"
 	"net/http"
+	"strconv"
+	"sync"
 
 	log "github.com/Sirupsen/logrus"
 	"github.com/q231950/sputnik/keymanager"
@@ -19,6 +23,12 @@ type CityParser struct {
 // Parse parses the file given input
 func (p CityParser) Parse(reader io.Reader) {
 	r := csv.NewReader(reader)
+
+	recordChannel := make(chan []string, 10)
+	wg := new(sync.WaitGroup)
+
+	go p.insertCity(recordChannel, wg)
+
 	for {
 		record, err := r.Read()
 		if err == io.EOF {
@@ -28,57 +38,111 @@ func (p CityParser) Parse(reader io.Reader) {
 			log.Fatal(err)
 		}
 
-		p.insertCity(record)
+		wg.Add(1)
+		recordChannel <- record
 	}
+
+	wg.Wait()
+
+	close(recordChannel)
 }
 
-func (p CityParser) insertCity(record []string) {
-	log.WithFields(log.Fields{
-		"Country code": record[0],
-		"City":         record[1],
-		"Accent city":  record[2],
-		"Region":       record[3],
-		"Population":   record[4],
-		"Latitude":     record[5],
-		"Longitude":    record[6]}).Info("record")
+func (p CityParser) insertCity(recordChannel chan []string, wg *sync.WaitGroup) {
 
-	keyManager := keymanager.New()
-	containerID := "iCloud.com.elbedev.bish"
-	config := requesthandling.RequestConfig{Version: "1", ContainerID: containerID}
-	subpath := "records/modify"
-	database := "public"
-	requestManager := requesthandling.New(config, &keyManager, database)
-	body := `{
-      "operations": [
-          {
-              "operationType": "create",
-              "record": {
-                  "recordType": "city",
-                  "fields": {
-                      "name": {
-                          "value": "bishkek"
-                      }
-                  }
-              }
-          }
-      ]
-  }`
-	request, err := requestManager.PostRequest(subpath, body)
-	if err == nil {
-		fmt.Println(request)
-	} else {
-		log.Fatal("Failed to create ping request")
+	for record := range recordChannel {
+		type City struct {
+			CountryCode, City, AccentCity, Region string
+			Population                            int
+			Latitude                              string
+			Longitude                             string
+		}
+
+		populationInt, _ := strconv.Atoi(record[4])
+		// latitudeFloat, _ := strconv.ParseFloat(record[5], 64)
+		// longitudeFloat, _ := strconv.ParseFloat(record[6], 64)
+		city := City{
+			record[0],
+			record[1],
+			record[2],
+			record[3],
+			populationInt,
+			record[5],
+			record[6]}
+		log.WithFields(log.Fields{
+			"Country code": city.CountryCode,
+			"City":         city.City,
+			"Accent city":  city.AccentCity,
+			"Region":       city.Region,
+			"Population":   city.Population,
+			"Latitude":     city.Latitude,
+			"Longitude":    city.Longitude}).Info("record")
+
+		keyManager := keymanager.New()
+		containerID := "iCloud.com.elbedev.bish"
+		config := requesthandling.RequestConfig{Version: "1", ContainerID: containerID}
+		subpath := "records/modify"
+		database := "public"
+		requestManager := requesthandling.New(config, &keyManager, database)
+		tmpl, err := template.New("test").Parse(`{
+		      "operations": [
+		          {
+		              "operationType": "create",
+		              "record": {
+		                  "recordType": "city",
+		                  "fields": {
+                          "country_code": {
+                            "value": "{{.CountryCode}}"
+                          },
+                          "accent_city":{
+                            "value": "{{.AccentCity}}"
+                          },
+		                      "name": {
+		                          "value": "{{.City}}"
+		                      },
+                          "region": {
+                            "value": "{{.Region}}"
+                          },
+                          "location": {
+                            "value": {
+                              "latitude": {{.Latitude}},
+                              "longitude": {{.Longitude}}
+                            }
+                          },
+                          "population": {
+                            "value": {{.Population}}
+                          }
+		                  }
+		              }
+		          }
+		      ]
+		  }`)
+		if err != nil {
+			panic(err)
+		}
+
+		var tpl bytes.Buffer
+		err = tmpl.Execute(&tpl, city)
+		if err != nil {
+			panic(err)
+		}
+		request, err := requestManager.PostRequest(subpath, tpl.String())
+		if err == nil {
+			fmt.Println(request)
+		} else {
+			log.Fatal("Failed to create ping request")
+		}
+
+		client := &http.Client{}
+		resp, err := client.Do(request)
+		if err != nil {
+			panic(err)
+		}
+		defer resp.Body.Close()
+
+		fmt.Println("response Status:", resp.Status)
+		fmt.Println("response Headers:", resp.Header)
+		responseBody, _ := ioutil.ReadAll(resp.Body)
+		fmt.Println("response Body:", string(responseBody))
+		wg.Done()
 	}
-
-	client := &http.Client{}
-	resp, err := client.Do(request)
-	if err != nil {
-		panic(err)
-	}
-	defer resp.Body.Close()
-
-	fmt.Println("response Status:", resp.Status)
-	fmt.Println("response Headers:", resp.Header)
-	responseBody, _ := ioutil.ReadAll(resp.Body)
-	fmt.Println("response Body:", string(responseBody))
 }
